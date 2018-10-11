838d496c93aa669063c17b2b97cb48bca02c1aa2
Lucho <sportelliluciano@gmail.com>
2018-10-11 20:24:53 -0300

    Agregado link a SDL en el cliente

--- src/cliente/main.cpp
+++ src/cliente/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "cliente/a.h"
 
 int main(int argc, char *argv[]) {
     A a;
+    SDL_Init(SDL_INIT_VIDEO);
     std::cout << "Hola cliente" << std::endl;
+    SDL_Quit();
     return 0;
 }
