92a4a669359ff04061ad84966fb8d533b648ec80
Lucho <sportelliluciano@gmail.com>
2018-10-15 13:59:18 -0300

    Fix en declaracion (agregado throw)

--- src/cliente/error_sdl.cpp
+++ src/cliente/error_sdl.cpp
@@ -8,6 +8,6 @@ ErrorSDL::ErrorSDL(const std::string& funcion_sdl) {
     mensaje = funcion_sdl + ": " + SDL_GetError();
 }
 
-const char *ErrorSDL::what() const {
+const char *ErrorSDL::what() const throw() {
     return mensaje.c_str();
 }
