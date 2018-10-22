e2354b57076a874b4ff3f2d6db78cbeea07422a0
santiago-alvarezjulia <santiago.alvarezjulia@gmail.com>
2018-10-22 16:18:43 -0300

    Hola Mundo Qt. Elimino include a.h del cliente para que no falle cmake

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <QApplication>
+#include <QLabel>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hola editor" << std::endl;
-    return 0;
+    // Clase que contiene el loop principal
+    QApplication app(argc, argv);
+    // Instancio un widget
+    // Llamo a QLabel(const QString &text, QWidget *parent=0, Qt::WindowFlags f=0);
+    QLabel label("Hello QT");
+    // Muestro el elemento
+    label.show();
+    // Arranca el loop de la UI
+    return app.exec();
 }
