11d0f17928fd9b907ab32ca57c2ee90fd157c978
Lucho <sportelliluciano@gmail.com>
2018-10-31 02:01:20 -0300

    Ahora muestra los FPS

--- src/cliente/modelo/hud.cpp
+++ src/cliente/modelo/hud.cpp
@@ -1,5 +1,8 @@
 #include "cliente/modelo/hud.h"
 
+#include <string>
+#include <sstream>
+
 #include "cliente/video/ventana.h"
 
 namespace cliente {
@@ -9,7 +12,15 @@ HUD::HUD() {
 }
 
 void HUD::renderizar(Ventana& ventana) {
+    std::stringstream s_fps;
+    s_fps << "FPS: " << ventana.fps();
 
+    Textura textura_fps = ventana
+        .obtener_administrador_texturas()
+        .crear_texto(s_fps.str());
+    
+    textura_fps.renderizar(ventana.ancho() - textura_fps.obtener_ancho() - 10,
+        10);
 }
 
 } // namespace cliente
