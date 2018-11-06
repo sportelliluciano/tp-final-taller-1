35f0af7740bb3ab9d86073ea8abe44cb8d4acac9
Lucho <sportelliluciano@gmail.com>
2018-11-06 10:55:48 -0300

    Movido inicialización de SDL a main

--- src/cliente/main.cpp
+++ src/cliente/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <exception>
 
+#include <SDL2/SDL.h>
+
 #include "cliente/cliente_juego.h"
 #include "cliente/servidor.h"
 
 int main(int argc, char *argv[]) {
+    SDL_Init(0);
     cliente::Servidor servidor(argc, argv);
     cliente::ClienteJuego cliente_juego(servidor, argc, argv);
     int retcode = EXIT_SUCCESS;
@@ -26,5 +29,6 @@ int main(int argc, char *argv[]) {
         // Preveer excepciones en caso de que no se pueda 
         //  hacerle join al hilo.
     }
+    SDL_Quit();
     return retcode;
 }
