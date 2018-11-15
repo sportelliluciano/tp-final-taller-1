b0b86f6cf164610d90e5231f3cdbe0fcc3dbf776
Lucho <sportelliluciano@gmail.com>
2018-11-15 20:30:48 -0300

    Agregada documentación

--- src/cliente/lanzador/lanzador.h
+++ src/cliente/lanzador/lanzador.h
@@ -21,14 +21,31 @@ private:
     Ui::Lanzador *ui;
     cliente::Servidor* servidor;
 
+    /**
+     * \brief Conecta los botones de la interfaz con sus funciones asociadas.
+     */
     void conectar_eventos();
 
+    /**
+     * \brief Los siguientes son los manejadores de la interfaz gráfica.
+     * 
+     * btn_*_click: Se ejecuta al hacer click en el botón *.
+     * list_salas_item_clicked: Se ejecuta al elegir una sala de la lista.
+     */
     void btn_iniciar_juego_click();
     void btn_conectar_click();
     void btn_actualizar_salas_click();
     void btn_elegir_salas_click();
     void btn_crear_sala_click();
     void list_salas_item_clicked();
+
+    /**
+     * \brief Se ejecutan cuando el usuario ingresa o sale de una sala.
+     * 
+     * Estos son eventos "falsos" ya que están generado desde el código.
+     * Los mismos se utilizan para actualizar la interfaz gráfica acorde a la
+     * situación.
+     */
     void al_ingresar_sala();
     void al_dejar_sala();
 
